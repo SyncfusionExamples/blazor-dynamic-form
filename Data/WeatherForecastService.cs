ecc604aa8292f31aabcf826aa9be6d75a011eba9
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2023-10-13 17:53:01 +0530

    Update the project

--- Data/WeatherForecastService.cs
+++ Data/WeatherForecastService.cs
@@ -1,24 +1,19 @@
-using System;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace FormBuilder.Data
+namespace DynamicFormBuilderSample.Data
 {
     public class WeatherForecastService
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
 
-        public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
+        public Task<WeatherForecast[]> GetForecastAsync(DateOnly startDate)
         {
-            var rng = new Random();
             return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = startDate.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             }).ToArray());
         }
     }
